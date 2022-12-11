f7c615e80853bc58a09c64412a3317c271450ba8
Omer-Demirtas <gamega55834@gmail.com>
2022-12-11 19:34:24 +0300

    Header component added.

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import "./Styles.css";
+
+const Header = () => {
+  return (
+    <div class="topnav">
+      <a class="active" href="#home">
+        React Reader
+      </a>
+    </div>
+  );
+};
+
+export default Header;
