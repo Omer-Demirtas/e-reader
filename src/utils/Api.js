5dae48b3e3b6c46ba7618ecb82377367805f7b57
Omer-Demirtas <gamega55834@gmail.com>
2022-12-13 00:35:04 +0300

    api header added

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
   baseURL: `${process.env.REACT_APP_BASE_URL}`,
   //headers: {'Authorization': `Bearer ${token}`}
 });
+
+/*
+  headers: {
+    "User-Agent": "PostmanRuntime/7.29.2",
+    Accept: "* / *",
+    "Postman-Token": "0ca3dc98-ca17-4c6d-89cd-e3e914533738",
+    Host: "firebasestorage.googleapis.com",
+    "Accept-Encoding": "gzip, deflate, br",
+    Connection: "keep-alive",
+  },
+*/
