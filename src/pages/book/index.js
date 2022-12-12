749c829462c824bab446124b33989c2a03b42cdb
Omer-Demirtas <gamega55834@gmail.com>
2022-12-12 21:46:33 +0300

    Book page added.

--- src/pages/book/index.js
+++ src/pages/book/index.js
@@ -1,14 +1,36 @@
+import Reader from "modules/reader/Reader";
 import { useParams } from "react-router-dom";
 
-const Book = () => 
-{
-    const { bookId } = useParams();
+const BOOK_NAMES = [
+  {
+    id: 1,
+    name: "Carl Sagan Kozmos",
+    url: "Carl Sagan - Kozmos__зЭ9х29.epub",
+  },
+  {
+    id: 2,
+    name: "Eski Bahce Tezer Ozlu",
+    url: "Eski Bahce - Tezer Ozlu.epub",
+  },
+];
 
-    console.log({ bookId });
+const Book = () => {
+  const { bookId } = useParams();
 
-    return (
-        <div>Boook</div>
-    );
-}
+  console.log({ bookId, b: BOOK_NAMES.find((b) => b.id === Number(bookId)) });
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "pink",
+      }}
+    >
+      <Reader url={`../${BOOK_NAMES.find((b) => b.id === Number(bookId)).url || ""}`} />
+    </div>
+  );
+};
 
 export default Book;
