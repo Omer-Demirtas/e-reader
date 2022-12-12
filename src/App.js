f03bc1102b80013d9a3d12f17a4568899f590c3f
Omer-Demirtas <gamega55834@gmail.com>
2022-12-13 00:35:18 +0300

    get boks when app init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
+import axios from "utils/Api";
+import React, { useEffect} from "react";
 import { BrowserRouter, useRoutes } from "react-router-dom";
 import { Routes } from "Routes";
+import { useBookStore } from "store/BookStore";
 
 function App() {
+  const setBooks = useBookStore(s => s.addAllBooks);
+  const books = useBookStore(s => s.books);
+
+  useEffect(() => {
+    const mapBooks = (books) => books.map(b => {
+      const final = {};
+      Object.keys(b.fields).forEach(f => final[f] = Object.values(b.fields[f])[0]);
+      return final;
+  });
+    const getBooks = async () => 
+    {
+      const books = await axios.get("book");
+      setBooks(mapBooks(books.data.documents));
+    }
+
+    getBooks();
+  }, []);
+
+  console.log({ books });
   return (
     <div className="App">
       <BrowserRouter>
