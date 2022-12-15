503c3f57a8922921628a73832ea9e49cfca0e90b
Omer-Demirtas <gamega55834@gmail.com>
2022-12-15 23:02:31 +0300

    Book Store updated.

--- src/store/BookStore.js
+++ src/store/BookStore.js
@@ -1,14 +1,12 @@
 import create from "zustand";
 
-export const useBookStore = create(
-  (set, get) =>
-    ({
-      books: [],
-      addBook: (book) => set((state) => ({ books: [...state.books, book] })),
-      addAllBooks: (books) => set(state => ({ ...state, books: [...state.books, ...books] })),
-      getBook: (id) => {
-        console.log(get().books);
-        return get().books.find((b) => b.id === id) || {}
-      },
-    } || {})
-);
+export const useBookStore = create((set, get) => ({
+  books: [],
+  addBook: (book) => set((state) => ({ books: [...state.books, book] })),
+  addAllBooks: (books) =>
+    set((state) => ({ ...state, books: [...state.books, ...books] })),
+  getBook: (id) => {
+    console.log(get().books);
+    return get().books.find((b) => b.id === id) || {};
+  },
+}));
