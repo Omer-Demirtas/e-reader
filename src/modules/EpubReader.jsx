05ecdae9aa2183c42ed51e17d3f8e6d8983ab75c
Omer-Demirtas <gamega55834@gmail.com>
2023-02-08 23:06:19 +0300

    Epub reader improvement.

--- src/modules/EpubReader.jsx
+++ src/modules/EpubReader.jsx
@@ -5,6 +5,7 @@ import { useReaderStore } from "store/ReaderStore";
 
 const EpubReader = ({ bookByte }) => {
   const ref = useRef();
+
   const currentCFI = useRef();
 
   const styles = useReaderStore(s => s.styles);
@@ -25,27 +26,24 @@ const EpubReader = ({ bookByte }) => {
 
   const onLocationChange = useCallback(
     (loc) => {
-      console.log({ loc, book });
-
       if (!book) return;
 
       //const spineItem = book.spine.get(startCfi);
       //const navItem = book.navigation.get(spineItem.href);
       //const chapterName = navItem && navItem.label.trim();
 
-      currentCFI.current = loc.start.cfi;
+      currentCFI.current = loc.start;
+
+      /*
       const locations = book.locations;
-      console.log({ locations, loc });
       const currentPage = locations.locationFromCfi(loc.start.cfi);
       const totalPage = locations.total;
       const total = book.locations.total;
+      */
       console.log({
-        total,
-        loc,
-        totalPage,
-        currentPage,
-        locations,
-        page: book.locations.locationFromCfi(loc.start.cfi),
+        cfi: loc.start,
+        currenct: currentCFI.current,
+        loc
       });
     },
     [rendition]
@@ -54,48 +52,47 @@ const EpubReader = ({ bookByte }) => {
   useEffect(() => {
     if(!bookByte) return;
     if(book) book.destroy();
+    if (rendition) rendition.destroy();
+
     console.log("MAIN");
 
     var book_ = null;
     var rendition_ = null;
-    book_ = new Epub();
-    book_.open(bookByte);
 
-    const view = ref.current;
+    const init = async () =>
+    {
+      book_ = new Epub({ fixedLayout: true });
+  
+      await book_.open(bookByte);
+  
+      const view = ref.current;
+  
+      rendition_ = book_.renderTo(view, {
+        flow: "scrolled-doc",
+        width: "100%",
+        fullsize: true
+      });
+      
+      rendition_.themes.default({
+        p: { "font-size": `${styles.fontSize}pt !important`, "line-height": "1.2", color: styles.textColor },
+        h6: { "font-size": `${styles.fontSize + 5}pt !important`,"line-height": "1.2" },
+        h5: { "font-size": `${styles.fontSize + 10}pt !important`, "line-height": "1.2", color: styles.textColor },
+        h4: { "font-size": `${styles.fontSize + 15}pt !important`, "line-height": "1.2", color: styles.textColor },
+        h3: { "font-size": `${styles.fontSize + 20}pt !important`, "line-height": "1.2", color: styles.textColor },
+        h2: { "font-size": `${styles.fontSize + 25}pt !important`, "line-height": "1.2", color: styles.textColor },
+        h1: { "font-size": `${styles.fontSize + 30}pt !important`, "line-height": "1.2", color: styles.textColor },
+      });
+  
+      rendition_.display(currentCFI.current);
 
-    rendition_ = book_.renderTo(view, {
-      width: '100%', 
-      height: '100%', 
-      method: "continuous",
-      flow: "scrolled",
-    });
-    
-    rendition_.themes.default({
-      p: { "font-size": `${styles.fontSize}pt !important`, "line-height": "1.2", color: styles.textColor },
-      h6: { "font-size": `${styles.fontSize + 5}pt !important`,"line-height": "1.2" },
-      h5: { "font-size": `${styles.fontSize + 10}pt !important`, "line-height": "1.2", color: styles.textColor },
-      h4: { "font-size": `${styles.fontSize + 15}pt !important`, "line-height": "1.2", color: styles.textColor },
-      h3: { "font-size": `${styles.fontSize + 20}pt !important`, "line-height": "1.2", color: styles.textColor },
-      h2: { "font-size": `${styles.fontSize + 25}pt !important`, "line-height": "1.2", color: styles.textColor },
-      h1: { "font-size": `${styles.fontSize + 30}pt !important`, "line-height": "1.2", color: styles.textColor },
-    });
+      setBook(book_);
+      setRendition(rendition_);
+    }
 
-    rendition_.display(currentCFI.current);
+    init();
+  }, [bookByte, styles]);
 
-    setBook(book_);
-    setRendition(rendition_);
-  }, [bookByte]);
-
-  useEffect(() => {
-    if (!rendition) return;
-
-    console.log("Location change re rendered.");
-
-    rendition.on("relocated", onLocationChange);
-    rendition.on("keyup", handleKeyPress);
-  }, [rendition]);
-
-  /** Viewer Option Changed */
+  /** Viewer Option Changed 
   useEffect(() => {
     let mounted = true;
     if (!book) return;
@@ -130,12 +127,9 @@ const EpubReader = ({ bookByte }) => {
 
         setRendition(rendition_);
 
-        console.log({ loc });
-        if (loc) {
-          rendition_.display(loc);
-        } else {
-          rendition_.display();
-        }
+        console.log({ loc, cfi: currentCFI.current });
+
+        rendition_.display(loc);
       }
     });
 
@@ -144,8 +138,23 @@ const EpubReader = ({ bookByte }) => {
       mounted = false;
     };
   }, [styles]);
+*/
+  
 
-  console.log({currentCFI: currentCFI.current});
+  useEffect(() => {
+    if(!rendition) return;
+
+    document.addEventListener('keyup', handleKeyPress, false);
+    rendition.on('keyup', handleKeyPress);
+    rendition.on('locationChanged', onLocationChange);
+
+    return () => {
+      document.removeEventListener('keyup', handleKeyPress, false);
+      rendition.off('keyup', handleKeyPress);
+      rendition.off('locationChanged', onLocationChange);
+    };
+  }, [rendition]);
+
 
   return (
     <div style={{ backgroundColor: styles.backgroundColor,  width: "100%", height: "100%" }}>
